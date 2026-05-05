2f80ccded7d96e2378eee24a606e91cfb9d142c1
Mateusz-Adamczak <161976386+Mateusz-Adamczak@users.noreply.github.com>
2026-05-05 11:32:25 +0200

    LevelUI: add PROCEDURE and ENDPROC to syntax highlighting

--- scripts/grammar/LevelUI.cs
+++ scripts/grammar/LevelUI.cs
@@ -208,9 +208,11 @@ public partial class LevelUI : Control
         highlighter.AddKeywordColor("ELSE", orange);
         highlighter.AddKeywordColor("WHILE", orange);
         highlighter.AddKeywordColor("REPEAT", orange);
+		highlighter.AddKeywordColor("PROCEDURE", orange);
 		highlighter.AddKeywordColor("ENDIF", orange);
 		highlighter.AddKeywordColor("ENDWHILE", orange);
 		highlighter.AddKeywordColor("ENDREPEAT", orange);
+		highlighter.AddKeywordColor("ENDPROC", orange);
 
         // other keywords
         highlighter.AddKeywordColor("LEFT", yellow);
